362e9c57c733c576205e40b5b67491ca8e041ffc
shawn edgell <shawn_edgell@hotmail.com>
2024-03-30 20:48:39 -0400

    layout fix

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -4,20 +4,21 @@
 	import SidebarCard from '$lib/components/SidebarCard.svelte';
 </script>
 
-<div class="mx-auto w-full flex justify-center">
+<div class="mx-auto w-full flex justify-between">
 	<!-- (Left Sidebar) -->
 	<div class="hidden md:flex fixed top-0 left-0 mt-[4.5rem] h-full w-[22rem]">
 		<SidebarLeft />
 	</div>
 	<!-- (Main Content) -->
-
-	<div class="md:ml-[22rem] space-y-5 p-4 xl:pr-0 w-full max-w-4xl">
-		<slot />
+	<div class="w-full flex justify-center">
+		<div class="md:ml-[22rem] space-y-5 p-4 xl:pr-0 w-full max-w-4xl">
+			<slot />
+		</div>
 	</div>
 	<!-- (Right Sidebar) -->
-	<div class="hidden xl:block sticky top-12 mb-16 h-full px-4 justify-end">
-		<div class="flex flex-col justify-end w-[22rem] space-y-6">
-			<div>
+	<div class="hidden xl:block sticky top-0 h-full px-4 justify-end">
+		<div class="flex flex-col justify-end w-[22rem]">
+			<div class="space-y-6 my-16">
 				<SidebarCard />
 				<TableOfContents>
 					<h1>On the Page</h1>
