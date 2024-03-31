891c6606ff88869ccd2790d4da38cd357fd491bb
shawn edgell <shawn_edgell@hotmail.com>
2024-03-31 01:56:22 -0400

    changed upload info position

--- src/routes/(app)/(games)/session/stats/+page.svelte
+++ src/routes/(app)/(games)/session/stats/+page.svelte
@@ -196,32 +196,6 @@
 		</div>
 		<div>
 			{#if session}
-				<aside class="alert variant-ghost-error mb-6">
-					<!-- Icon -->
-					<div>
-						<svg class="w-12 h-12" viewBox="0 0 24 24" fill="none">
-							<path
-								d="M12 17.0001H12.01M12 10.0001V14.0001M6.41209 21.0001H17.588C19.3696 21.0001 20.2604 21.0001 20.783 20.6254C21.2389 20.2985 21.5365 19.7951 21.6033 19.238C21.6798 18.5996 21.2505 17.819 20.3918 16.2579L14.8039 6.09805C13.8897 4.4359 13.4326 3.60482 12.8286 3.32987C12.3022 3.09024 11.6978 3.09024 11.1714 3.32987C10.5674 3.60482 10.1103 4.4359 9.19614 6.09805L3.6082 16.2579C2.74959 17.819 2.32028 18.5996 2.39677 19.238C2.46351 19.7951 2.76116 20.2985 3.21709 20.6254C3.7396 21.0001 4.63043 21.0001 6.41209 21.0001Z"
-								stroke="currentColor"
-								stroke-width="2"
-								stroke-linecap="round"
-								stroke-linejoin="round"
-							/>
-						</svg>
-					</div>
-					<!-- Message -->
-					<div class="alert-message">
-						<h3 class="h3">Upload Info</h3>
-						<p>
-							Please note that you can currently only upload up to 8 images at a time. This will
-							likely change in the future. For now, we recommend splitting your Stats and Camera
-							settings into two different uploads. We also recommend images to be 1280x720, but any
-							size will work as long as the images are less than 2MB each. You can always edit or
-							delete your stat uploads.
-						</p>
-					</div>
-				</aside>
-
 				<form
 					class="flex flex-col space-y-5"
 					method="post"
@@ -231,7 +205,31 @@
 					enctype="multipart/form-data"
 				>
 					<h2>Upload Your Stats</h2>
-
+					<aside class="alert variant-ghost-error mb-6">
+						<!-- Icon -->
+						<div>
+							<svg class="w-12 h-12" viewBox="0 0 24 24" fill="none">
+								<path
+									d="M12 17.0001H12.01M12 10.0001V14.0001M6.41209 21.0001H17.588C19.3696 21.0001 20.2604 21.0001 20.783 20.6254C21.2389 20.2985 21.5365 19.7951 21.6033 19.238C21.6798 18.5996 21.2505 17.819 20.3918 16.2579L14.8039 6.09805C13.8897 4.4359 13.4326 3.60482 12.8286 3.32987C12.3022 3.09024 11.6978 3.09024 11.1714 3.32987C10.5674 3.60482 10.1103 4.4359 9.19614 6.09805L3.6082 16.2579C2.74959 17.819 2.32028 18.5996 2.39677 19.238C2.46351 19.7951 2.76116 20.2985 3.21709 20.6254C3.7396 21.0001 4.63043 21.0001 6.41209 21.0001Z"
+									stroke="currentColor"
+									stroke-width="2"
+									stroke-linecap="round"
+									stroke-linejoin="round"
+								/>
+							</svg>
+						</div>
+						<!-- Message -->
+						<div class="alert-message">
+							<h3 class="h3">Upload Info</h3>
+							<p>
+								Please note that you can currently only upload up to 8 images at a time. This will
+								likely change in the future. For now, we recommend splitting your Stats and Camera
+								settings into two different uploads. We also recommend images to be 1280x720, but
+								any size will work as long as the images are less than 2MB each. You can always edit
+								or delete your stat uploads.
+							</p>
+						</div>
+					</aside>
 					<div class="w-full">
 						<input
 							class="input"
--- src/routes/(app)/(games)/skaterxl/stats/+page.svelte
+++ src/routes/(app)/(games)/skaterxl/stats/+page.svelte
@@ -202,31 +202,6 @@
 		</div>
 		<div>
 			{#if session}
-				<aside class="alert variant-ghost-error mb-6">
-					<!-- Icon -->
-					<div>
-						<svg class="w-12 h-12" viewBox="0 0 24 24" fill="none">
-							<path
-								d="M12 17.0001H12.01M12 10.0001V14.0001M6.41209 21.0001H17.588C19.3696 21.0001 20.2604 21.0001 20.783 20.6254C21.2389 20.2985 21.5365 19.7951 21.6033 19.238C21.6798 18.5996 21.2505 17.819 20.3918 16.2579L14.8039 6.09805C13.8897 4.4359 13.4326 3.60482 12.8286 3.32987C12.3022 3.09024 11.6978 3.09024 11.1714 3.32987C10.5674 3.60482 10.1103 4.4359 9.19614 6.09805L3.6082 16.2579C2.74959 17.819 2.32028 18.5996 2.39677 19.238C2.46351 19.7951 2.76116 20.2985 3.21709 20.6254C3.7396 21.0001 4.63043 21.0001 6.41209 21.0001Z"
-								stroke="currentColor"
-								stroke-width="2"
-								stroke-linecap="round"
-								stroke-linejoin="round"
-							/>
-						</svg>
-					</div>
-					<!-- Message -->
-					<div class="alert-message">
-						<h3 class="h3">Upload Info</h3>
-						<p>
-							Please make sure to organize your stats in a .zip file and include a description for
-							each file so users will understand their purpose. You can also include a text file if
-							you think it might provide helpful additional information. Remember, you can always
-							edit or delete your stat uploads.
-						</p>
-					</div>
-				</aside>
-
 				<form
 					class="flex flex-col space-y-5"
 					method="post"
@@ -236,6 +211,30 @@
 					enctype="multipart/form-data"
 				>
 					<h2>Upload Your Stats</h2>
+					<aside class="alert variant-ghost-error mb-6">
+						<!-- Icon -->
+						<div>
+							<svg class="w-12 h-12" viewBox="0 0 24 24" fill="none">
+								<path
+									d="M12 17.0001H12.01M12 10.0001V14.0001M6.41209 21.0001H17.588C19.3696 21.0001 20.2604 21.0001 20.783 20.6254C21.2389 20.2985 21.5365 19.7951 21.6033 19.238C21.6798 18.5996 21.2505 17.819 20.3918 16.2579L14.8039 6.09805C13.8897 4.4359 13.4326 3.60482 12.8286 3.32987C12.3022 3.09024 11.6978 3.09024 11.1714 3.32987C10.5674 3.60482 10.1103 4.4359 9.19614 6.09805L3.6082 16.2579C2.74959 17.819 2.32028 18.5996 2.39677 19.238C2.46351 19.7951 2.76116 20.2985 3.21709 20.6254C3.7396 21.0001 4.63043 21.0001 6.41209 21.0001Z"
+									stroke="currentColor"
+									stroke-width="2"
+									stroke-linecap="round"
+									stroke-linejoin="round"
+								/>
+							</svg>
+						</div>
+						<!-- Message -->
+						<div class="alert-message">
+							<h3 class="h3">Upload Info</h3>
+							<p>
+								Please make sure to organize your stats in a .zip file and include a description for
+								each file so users will understand their purpose. You can also include a text file
+								if you think it might provide helpful additional information. Remember, you can
+								always edit or delete your stat uploads.
+							</p>
+						</div>
+					</aside>
 
 					<div class="w-full">
 						<input
