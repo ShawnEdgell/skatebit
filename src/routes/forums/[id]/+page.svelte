5fcb8a7dab808a604c948a0620f8c6f9049354db
shawn edgell <shawn_edgell@hotmail.com>
2024-04-15 03:34:43 -0400

    nearly full functional forums, just need to figure out delete/edit functionality

--- src/routes/forums/[id]/+page.svelte
+++ src/routes/forums/[id]/+page.svelte
@@ -1,9 +1,6 @@
-<!-- YourComponent.svelte -->
-
 <script lang="ts">
-	import { onMount } from 'svelte';
+	import { onMount, afterUpdate } from 'svelte';
 
-	// Define the Thread and Profile interfaces
 	interface Thread {
 		id: number;
 		title: string;
@@ -21,10 +18,9 @@
 		id: number;
 		comment_text: string;
 		profiles: Profile;
-		created_at: string; // Add created_at to Comment interface
+		created_at: string;
 	}
 
-	// Define the component's props
 	export let data: {
 		session: any;
 		supabase: any;
@@ -32,37 +28,30 @@
 		comments: Comment[];
 	};
 
-	// Define component variables and functions
 	let { session, supabase, thread, comments } = data;
 	let newComment = '';
 	let error: string | null = null;
 	let loading = false;
+	let commentsContainer: HTMLElement | null = null;
 
-	// Modify the fetchComments function to populate the profile data
-	const fetchComments = async () => {
+	const fetchCommentsAndSubmit = async () => {
 		try {
 			const { data: commentsData, error: fetchError } = await supabase
 				.from('comments')
-				.select('id, comment_text, profile_id, profiles(username), created_at') // Fetch created_at column
+				.select('id, comment_text, profile_id, profiles(username), created_at')
 				.eq('thread_id', thread.id);
 
-			if (fetchError) {
-				console.error('Error fetching comments:', fetchError.message);
-				throw new Error('Failed to fetch comments');
-			}
+			if (fetchError) throw new Error(fetchError.message);
 
-			// Map comments data to include profile information
-			comments = commentsData.map((comment: Comment) => {
-				return {
-					...comment,
-					profile: comment.profiles ? { username: comment.profiles.username } : null
-				};
-			});
+			comments = commentsData.map((comment: Comment) => ({
+				...comment,
+				profile: comment.profiles ? { username: comment.profiles.username } : null
+			}));
 
 			error = null;
 		} catch (error: any) {
-			console.error('Failed to fetch comments:', error.message);
-			error = 'Failed to fetch comments. Please try again later.';
+			console.error('Error:', error.message);
+			error = error.message || 'An error occurred.';
 		}
 	};
 
@@ -87,40 +76,43 @@
 				}
 			]);
 
-			if (insertError) {
-				console.error('Error adding comment:', insertError.message);
-				throw new Error('Failed to add comment');
-			}
+			if (insertError) throw new Error(insertError.message);
 
-			await fetchComments();
+			await fetchCommentsAndSubmit();
 			newComment = '';
-			error = null;
 		} catch (error: any) {
-			console.error('Failed to add comment:', error.message);
-			error = 'Failed to add comment. Please try again later.';
+			console.error('Error:', error.message);
+			error = error.message || 'An error occurred.';
 		} finally {
 			loading = false;
 		}
 	};
 
-	// Run code on component mount
-	onMount(fetchComments);
+	onMount(fetchCommentsAndSubmit);
 
-	// Function to format timestamp
-	function formatDate(timestamp: string) {
+	afterUpdate(() => {
+		if (commentsContainer) {
+			commentsContainer.scrollTop = commentsContainer.scrollHeight;
+		}
+	});
+
+	const formatDate = (timestamp: string) => {
 		const date = new Date(timestamp);
-
-		const options: Intl.DateTimeFormatOptions = {
+		return date.toLocaleDateString('en-US', {
 			month: 'long',
 			day: 'numeric',
 			hour: 'numeric',
 			minute: 'numeric',
 			hour12: true
-		};
-		const formattedDate = date.toLocaleDateString('en-US', options);
+		});
+	};
 
-		return formattedDate;
-	}
+	const handleKeyDown = (event: KeyboardEvent) => {
+		if (event.key === 'Enter') {
+			event.preventDefault();
+			submitComment();
+		}
+	};
 </script>
 
 <svelte:head>
@@ -128,33 +120,29 @@
 </svelte:head>
 
 <article
-	class="container absolute mx-auto max-w-4xl inset-x-0 top-[4.5rem] bottom-0 flex h-screen flex-col p-4"
+	class="container absolute mx-auto max-w-4xl inset-x-0 top-[4.6rem] bottom-0 flex h-screen flex-col p-4"
 >
-	<!-- Forum Topic (Stuck to Top) -->
-	<div class="header mb-2">
+	<div class="header">
 		<span class="badge variant-filled-primary mb-2">Thread</span>
 		<h1>{thread.title}</h1>
 		<p>{thread.description}</p>
 		<hr class="!border-t-2" />
 	</div>
 
-	<div class="flex-grow overflow-y-auto mb-12">
+	<div class="flex-grow overflow-y-auto whitespace-wrap mb-12" bind:this={commentsContainer}>
 		<ul>
 			{#each comments as comment}
 				<li class="py-2">
 					<div class="flex items-center mb-1">
 						<span class="mr-2">{comment.profiles.username}</span>
-						<!-- Use 'profiles' instead of 'profile' -->
 						<span class="text-gray-500 text-xs">{formatDate(comment.created_at)}</span>
-						<!-- Display casual timestamp -->
 					</div>
-					<div class="text-base">{comment.comment_text}</div>
+					<div class="text-base whitespace-wrap break-all">{comment.comment_text}</div>
 				</li>
 			{/each}
 		</ul>
 	</div>
 
-	<!-- Comment Input and Post Button (Stuck to Bottom) -->
 	{#if session}
 		<div class="input-container mb-0 flex sticky bottom-0 py-4">
 			<input
@@ -162,6 +150,7 @@
 				bind:value={newComment}
 				placeholder="Add a comment..."
 				class="input mr-2 flex-grow"
+				on:keydown={handleKeyDown}
 			/>
 			<button
 				on:click={submitComment}
@@ -173,7 +162,6 @@
 		<p class="py-4">Please log in to post comments.</p>
 	{/if}
 
-	<!-- Error Message -->
 	{#if error}
 		<p class="text-red-500 p-4">{error}</p>
 	{/if}
