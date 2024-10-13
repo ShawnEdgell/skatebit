aa7ba18e698c0736d1149cde72f5c3cdb55f56a9
ShawnEdgell <milky.irl@gmail.com>
2024-10-13 15:55:53 -0400

    updated focus trap

--- src/components/DrawerMenu.svelte
+++ src/components/DrawerMenu.svelte
@@ -9,10 +9,18 @@
 </script>
 
 <Drawer id="main-menu">
+	<!-- Add a hidden div with a focus index so it gets focused first -->
+	<div data-focusindex="0" tabindex="-1" aria-hidden="true"></div>
+
 	<ul class="p-4 space-y-2">
-		{#each navItems as { href, label }}
+		{#each navItems as { href, label }, index}
 			<li>
-				<a {href} class="btn font-semibold w-full variant-ghost" on:click={closeDrawer}>
+				<a
+					{href}
+					class="btn font-semibold w-full variant-ghost"
+					on:click={closeDrawer}
+					data-focusindex={index + 1}
+				>
 					{label}
 				</a>
 			</li>
