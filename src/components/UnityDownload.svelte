78d3429226958ed0a058a39e077fc084fce48b40
ShawnEdgell <milky.irl@gmail.com>
2024-10-13 20:44:15 -0400

    moved umm downloads to firebase

--- src/components/UnityDownload.svelte
+++ src/components/UnityDownload.svelte
@@ -9,7 +9,7 @@
 		<svelte:fragment slot="content">
 			<a
 				class="btn mt-2 variant-filled-secondary no-underline"
-				href="https://jvfiwbxwwjhhbfonzgbc.supabase.co/storage/v1/object/public/UnityModManager/Alpha/UnityModManager-21-0-27-4-1690201835.zip"
+				href="https://firebasestorage.googleapis.com/v0/b/stillmilkylive.appspot.com/o/UnityModManager-21-0-27-4-1690201835.zip?alt=media&token=2c4bbeeb-5c1c-4d41-8a5e-5d7774e2b507"
 			>
 				Download UnityModManager v0.27.4
 			</a></svelte:fragment
@@ -21,7 +21,7 @@
 		<svelte:fragment slot="content">
 			<a
 				class="btn mt-2 variant-filled-secondary no-underline"
-				href="https://jvfiwbxwwjhhbfonzgbc.supabase.co/storage/v1/object/public/UnityModManager/Public/UnityModManager-21-0-27-7-1694510711.zip"
+				href="https://firebasestorage.googleapis.com/v0/b/stillmilkylive.appspot.com/o/UnityModManager-21-0-27-7-1694510711.zip?alt=media&token=5dd6cb37-3074-4b51-b84e-38ac6290bb9d"
 			>
 				Download UnityModManager v0.27.7
 			</a></svelte:fragment
