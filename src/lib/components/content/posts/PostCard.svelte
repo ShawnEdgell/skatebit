ac525fc41881e52b240df684b327d05922bcba34
ShawnEdgell <milky.irl@gmail.com>
2024-11-02 20:05:32 -0400

    updated post previews

--- src/lib/components/content/posts/PostCard.svelte
+++ src/lib/components/content/posts/PostCard.svelte
@@ -4,27 +4,26 @@
 	export let post: Post;
 </script>
 
-<div class="not-prose mt-4 space-y-2">
-	<h3 class="text-xl lg:text-2xl">{post.title}</h3>
-	<div class="flex items-center gap-2 text-sm lg:text-base">
-		{#if post.userPhotoURL}
-			<img
-				src={post.userPhotoURL}
-				alt="{post.userName}'s profile picture"
-				class="w-6 m-0 rounded-full"
-			/>
-		{/if}
+<a href="/posts/{post.id}" class="card bg-base-200 hover:bg-base-300 border shadow-lg not-prose">
+	<div class="card-body">
+		<h2 class="card-title">{post.title}</h2>
+		<div class="flex items-center gap-2 text-sm lg:text-base">
+			{#if post.userPhotoURL}
+				<img
+					src={post.userPhotoURL}
+					alt="{post.userName}'s profile picture"
+					class="w-6 m-0 rounded-full"
+				/>
+			{/if}
 
-		<p>
-			Posted by {post.userName} on {new Date(post.createdAt.seconds * 1000).toLocaleString()}
-		</p>
+			<p>
+				Posted by {post.userName} on {new Date(post.createdAt.seconds * 1000).toLocaleString()}
+			</p>
+		</div>
+		<section>
+			<p>
+				{post.description.length > 100 ? `${post.description.slice(0, 100)}...` : post.description}
+			</p>
+		</section>
 	</div>
-	<section>
-		<p>
-			{post.description.length > 100 ? `${post.description.slice(0, 100)}...` : post.description}
-		</p>
-	</section>
-	<div>
-		<a class="btn btn-neutral mb-2" href="/posts/{post.id}">View Post</a>
-	</div>
-</div>
+</a>
