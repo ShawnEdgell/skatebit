cc7af93a24382c58f0b43a1a41c4e9a5310df79b
shredgell <shawn.edgell.github@gmail.com>
2025-03-26 12:12:38 -0400

    readded missing component

---
+++ src/lib/components/Card.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	export let title: string;
+	export let content: string = '';
+	export let download: string = '';
+</script>
+
+<div class="card card-lg bg-base-200 not-prose">
+	<div class="card-body">
+		<h2 class="card-title prose">{title}</h2>
+		{#if content}
+			<p class="prose">{content}</p>
+		{/if}
+		{#if download}
+			<div class="flex w-full justify-end">
+				<a class="btn btn-primary" href={download}>Download</a>
+			</div>
+		{/if}
+		<slot />
+	</div>
+</div>
