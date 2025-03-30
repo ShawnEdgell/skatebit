f55470915edb7d6f1cd58cf1dd5fd15ead732ef8
shredgell <shawn.edgell.github@gmail.com>
2025-03-30 03:54:20 -0400

    fixed alertbar

--- src/lib/components/AlertBar.svelte
+++ src/lib/components/AlertBar.svelte
@@ -1,8 +1,12 @@
-<div class="navbar bg-primary text-primary-content flex justify-between px-4">
+<a href="/cotw" class="navbar bg-primary flex justify-center px-4 md:hidden">
 	<p class="text-sm md:pl-2 md:text-base">
 		Submit your best clips on our new <b>Clip of the Week</b> page!
 	</p>
-	<div>
-		<a href="/cotw" class="btn btn-sm md:btn-md">CoTW</a>
-	</div>
+</a>
+
+<div class="navbar bg-primary hidden justify-between px-4 md:flex">
+	<p class="text-sm md:pl-2 md:text-base">
+		Submit your best clips on our new <b>Clip of the Week</b> page!
+	</p>
+	<button class="btn">Clip of the Week</button>
 </div>
