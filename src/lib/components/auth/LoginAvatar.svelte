e8c9ab35559e8d52e6541ed7477d32c96559c81f
shredgell <shawn.edgell.github@gmail.com>
2025-04-02 16:12:21 -0400

    fixed avatar login and logout on mobile

--- src/lib/components/auth/LoginAvatar.svelte
+++ src/lib/components/auth/LoginAvatar.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <div class="dropdown dropdown-end">
-	<button class="btn btn-circle avatar btn-sm" tabindex="0">
+	<label tabindex="0" class="btn btn-circle avatar h-8 w-8">
 		{#if $user}
 			<img
 				class="rounded-full"
@@ -17,18 +17,17 @@
 				fill="currentColor"
 				viewBox="-8 0 512 512"
 				xmlns="http://www.w3.org/2000/svg"
-				stroke="white"
-				aria-hidden="true"
+				stroke="#ffffff"
 			>
 				<path
 					d="M248 8C111 8 0 119 0 256s111 248 248 248 248-111 248-248S385 8 248 8zm0 96c48.6 0 88 39.4 88 88s-39.4 88-88 88-88-39.4-88-88 39.4-88 88-88zm0 344c-58.7 0-111.3-26.6-146.5-68.2 18.8-35.4 55.6-59.8 98.5-59.8 2.4 0 4.8.4 7.1 1.1 13 4.2 26.6 6.9 40.9 6.9 14.3 0 28-2.7 40.9-6.9 2.3-.7 4.7-1.1 7.1-1.1 42.9 0 79.7 24.4 98.5 59.8C359.3 421.4 306.7 448 248 448z"
 				/>
 			</svg>
 		{/if}
-	</button>
-
+	</label>
 	<ul
-		class="menu dropdown-content bg-base-200 rounded-box absolute right-0 z-[9999] mt-5 w-56 p-4 shadow"
+		tabindex="0"
+		class="menu menu-md dropdown-content bg-base-200 rounded-box z-[1] mt-5 w-56 p-4 shadow"
 	>
 		<li><GoogleLoginButton /></li>
 	</ul>
